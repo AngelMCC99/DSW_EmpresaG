3c07bec92c441efe2f62302f71a4bdb387e5a32c
AngelMCC99 <57779761+AngelMCC99@users.noreply.github.com>
2020-11-14 21:45:36 -0430

    Login
    .

--- MercadeoUCAB-frontend/src/app/services/login.service.ts
+++ MercadeoUCAB-frontend/src/app/services/login.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Person } from '../classes/person';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { baseURL } from '../constants/baseURL';
+import { map, catchError } from 'rxjs/operators';
+import { ProcessHttpMessageService } from '../services/process-http-message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+
+  user: Person = {
+    correo_electronico: '',
+    clave: ''
+  };
 
   constructor() { }
 }
